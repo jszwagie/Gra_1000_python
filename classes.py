33a5994278dc3c3a81a505e3f1672eaea04d61d4
Jan Szwagierczak <01187293@pw.edu.pl>
2023-12-15 18:04:47 +0100

    added class Player and Musik and tests for that classes

--- classes.py
+++ classes.py
@@ -41,3 +41,40 @@ class Deck:
     @property
     def deck(self):
         return self._deck
+
+
+class Player:
+    def __init__(self):
+        self._hand = []
+        self._points = 0
+
+    def add_card(self, card):
+        self._hand.append(card)
+
+    def play_card(self, card):
+        if card in self._hand:
+            self._hand.remove(card)
+            return card
+        else:
+            return None
+
+    def add_from_musik(self, musik):
+        for card in musik:
+            self._hand.append(card)
+
+    def show_hand(self):
+        list_of_cards = []
+        for card in self._hand:
+            list_of_cards.append(f'{card.name} of {card.suit}')
+        return list_of_cards
+
+
+class Musik:
+    def __init__(self):
+        self._cards = []
+
+    def add_card(self, card):
+        self._cards.append(card)
+
+    def cards_in_musik(self):
+        return self._cards
--- test_classes.py
+++ test_classes.py
@@ -1,6 +1,7 @@
-from classes import Card, Deck
+from classes import Card, Deck, Player, Musik
 
 
+# Testy dla klasy Card
 def test_Card_init_and_properties():
     queen = Card('Queen', 'Spades', 11)
     assert queen.name == 'Queen'
@@ -8,6 +9,7 @@ def test_Card_init_and_properties():
     assert queen.suit == 'Spades'
 
 
+# Testy dla klasy Deck
 def test_generate_deck():
     deck_1 = Deck()
     deck_1.generate_deck()
@@ -21,3 +23,73 @@ def test_schuffle_deck():
     deck_1.shuffle_deck()
     assert original != deck_1.deck
     assert len(original) == len(deck_1.deck)
+
+
+# Testy dla klasy Player
+def test_Player_add_card():
+    player = Player()
+    card = Card("Ace", "Spades", 11)
+    player.add_card(card)
+    assert len(player._hand) == 1
+    assert player._hand[0] == card
+
+
+def test_Player_play_card():
+    player = Player()
+    card_1 = Card("Ace", "Spades", 11)
+    card_2 = Card("King", "Hearts", 4)
+    player.add_card(card_1)
+    player.add_card(card_2)
+    played_card = player.play_card(card_1)
+    assert played_card == card_1
+    assert len(player._hand) == 1
+
+
+def test_Player_play_card_not_in_hand():
+    player = Player()
+    card_1 = Card("Ace", "Spades", 11)
+    card_2 = Card("10", "Diamonds", 10)
+    player.add_card(card_1)
+    played_card = player.play_card(card_2)
+    assert played_card is None
+    assert len(player._hand) == 1
+
+
+def test_add_from_musik():
+    player = Player()
+    musik = [Card("Ace", "Spades", 11), Card("10", "Hearts", 10)]
+    player.add_from_musik(musik)
+    assert len(player._hand) == 2
+
+
+def test_show_hand():
+    player = Player()
+    card_1 = Card("Ace", "Spades", 11)
+    card_2 = Card("10", "Hearts", 10)
+    player.add_card(card_1)
+    player.add_card(card_2)
+    hand = player.show_hand()
+    assert len(hand) == 2
+    assert 'Ace of Spades' in hand
+    assert '10 of Hearts' in hand
+
+
+# Testy dla klasy Musik
+def test_add_card_to_musik():
+    musik = Musik()
+    card = Card("Ace", "Spades", 11)
+    musik.add_card(card)
+    assert len(musik.cards_in_musik()) == 1
+    assert musik.cards_in_musik()[0] == card
+
+
+def test_cards_in_musik():
+    musik = Musik()
+    card_1 = Card("Ace", "Spades", 11)
+    card_2 = Card("10", "Hearts", 10)
+    musik.add_card(card_1)
+    musik.add_card(card_2)
+    cards_in_musik = musik.cards_in_musik()
+    assert len(cards_in_musik) == 2
+    assert card_1 in cards_in_musik
+    assert card_2 in cards_in_musik
