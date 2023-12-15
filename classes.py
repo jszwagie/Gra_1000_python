9005742130a86e590dd1c6304492089255e957c6
Jan Szwagierczak <01187293@pw.edu.pl>
2023-12-15 17:18:56 +0100

    Added class Deck and tests for that class

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from random import shuffle
 
 
 @dataclass
@@ -18,3 +19,25 @@ class Card:
     @property
     def points(self):
         return self._points
+
+
+class Deck:
+    def __init__(self):
+        self._deck = []
+
+    def generate_deck(self):
+        deck = []
+        cards = {'Ace': 11, '10': 10, 'King': 4, 'Queen': 3, 'Jack': 2, '9': 0}
+        suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+        for suit in suits:
+            for figure, points in cards.items():
+                card = Card(figure, suit, points)
+                deck.append(card)
+        self._deck = deck
+
+    def shuffle_deck(self):
+        shuffle(self._deck)
+
+    @property
+    def deck(self):
+        return self._deck
--- test_classes.py
+++ test_classes.py
@@ -1,4 +1,4 @@
-from classes import Card
+from classes import Card, Deck
 
 
 def test_Card_init_and_properties():
@@ -6,3 +6,18 @@ def test_Card_init_and_properties():
     assert queen.name == 'Queen'
     assert queen.points == 11
     assert queen.suit == 'Spades'
+
+
+def test_generate_deck():
+    deck_1 = Deck()
+    deck_1.generate_deck()
+    assert len(deck_1.deck) == 24
+
+
+def test_schuffle_deck():
+    deck_1 = Deck()
+    deck_1.generate_deck()
+    original = deck_1.deck.copy()
+    deck_1.shuffle_deck()
+    assert original != deck_1.deck
+    assert len(original) == len(deck_1.deck)
