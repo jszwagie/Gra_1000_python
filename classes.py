ea01a72600ed30449f15e11b8a6ec9c7d4998414
Jan Szwagierczak <01187293@pw.edu.pl>
2023-12-23 01:41:29 +0100

    updated requirements.txt, deleted unused functions and turn couple functions private

--- classes.py
+++ classes.py
@@ -20,10 +20,6 @@ class Card:
     def points(self):
         return self._points
 
-    def __str__(self):
-        desc = f'{self._name} {self._suit}'
-        return desc
-
     def __eq__(self, other):
         if (self._name == other._name and self._suit == other._suit and
            self._points == other._points):
@@ -77,9 +73,6 @@ class Player:
     def hand(self):
         return self._hand
 
-    def add_card(self, card):
-        self._hand.append(card)
-
     def play_card(self, card_number):
         card = self._hand[card_number]
         self._hand.remove(card)
@@ -88,26 +81,10 @@ class Player:
     def add_from_musik(self, musik):
         for card in musik.cards_in_musik():
             self._hand.append(card)
-        musik.clear()
-
-    def show_hand(self):
-        list_of_cards = []
-        for card in self._hand:
-            list_of_cards.append(str(card))
-        return list_of_cards
-
-    def cards_display(self):
-        list_of_cards = self.show_hand()
-        result = "Yours cards: " + ', '.join(list_of_cards) + '.'
-        return result
+        musik._clear()
 
     def set_bid(self, bid):
         self._bid = bid
-
-    def remove_card(self, number):
-        card = self._hand[number]
-        self._hand.remove(card)
-        return card
 
     def remove_after_musik(self, cards):
         cards_discard = []
@@ -117,10 +94,10 @@ class Player:
             self._hand.remove(card)
         return cards_discard
 
-    def add_points(self, points):
+    def _add_points(self, points):
         self._points += points
 
-    def have_trump(self):
+    def _have_trump(self):
         trumps = []
         suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
         for suit in suits:
@@ -130,14 +107,14 @@ class Player:
                 trumps.append(suit)
         return trumps
 
-    def set_trumps(self):
-        trumps = self.have_trump()
+    def _set_trumps(self):
+        trumps = self._have_trump()
         self._trumps = trumps
 
-    def trump_played(self, trump):
+    def _trump_played(self, trump):
         self._trumps.remove(trump)
 
-    def suit_in_hand(self, suit):
+    def _suit_in_hand(self, suit):
         for card in self._hand:
             if card.suit == suit:
                 return True
@@ -157,17 +134,10 @@ class Musik(Deck):
     def __init__(self):
         super().__init__()
 
-    def add_card(self, card):
-        self._cards.append(card)
-
     def cards_in_musik(self):
         return self._cards
 
-    def __str__(self):
-        result = f'{self._cards[0]}, {self._cards[1]}'
-        return result
-
-    def clear(self):
+    def _clear(self):
         self._cards.clear()
 
 
@@ -177,7 +147,7 @@ class Computer(Player):
         self._temporary_trump = ''
         self._trump_declared = False
 
-    def pri_first(self, card):
+    def _pri_first(self, card):
         if card.name == 'Ace':
             return -1
         elif card.suit in self._trumps and card.name in ["Queen", "King"]:
@@ -185,7 +155,7 @@ class Computer(Player):
         else:
             return card.points
 
-    def pri_suit(self, card, suit, game):
+    def _pri_suit(self, card, suit, game):
         priority = 0
         if card.suit == suit:
             priority += 100
@@ -199,16 +169,16 @@ class Computer(Player):
         trumps = self._trumps
         if game.active_trump != self._temporary_trump:
             self._temporary_trump = ''
-        if not self.suit_in_hand(self._temporary_trump):
+        if not self._suit_in_hand(self._temporary_trump):
             if len(trumps) > 1:
-                trumps = sorted(trumps, key=lambda x: game.trump_value(x),
+                trumps = sorted(trumps, key=lambda x: game._trump_value(x),
                                 reverse=True)
             if trumps:
                 self._temporary_trump = trumps[0]
         if opponent_card:
             base_suit = opponent_card.suit
             pri_cards = sorted(hand, key=lambda x:
-                               self.pri_suit(x, base_suit, game),
+                               self._pri_suit(x, base_suit, game),
                                reverse=True)
             card_to_play_index = hand.index(pri_cards[0])
         else:
@@ -217,23 +187,23 @@ class Computer(Player):
                 card_to_play_index = hand.index(card)
                 self._trump_declared = True
             else:
-                pri_cards = sorted(hand, key=lambda x: self.pri_first(x))
+                pri_cards = sorted(hand, key=lambda x: self._pri_first(x))
                 card_to_play_index = hand.index(pri_cards[0])
         played_card = self.play_card(card_to_play_index)
         return played_card
 
-    def points_from_trumps(self):
-        trumps = self.have_trump()
+    def _points_from_trumps(self):
+        trumps = self._have_trump()
         points = 0
         suits = {'Spades': 40, 'Hearts': 100, 'Clubs': 60, 'Diamonds': 80}
         for element in trumps:
             points += suits[element]
         return points
 
-    def possible_points(self, opponent_bid):
+    def _possible_points(self, opponent_bid):
         possible_points = 0
-        trump_points = self.points_from_trumps()
-        trumps = self.have_trump()
+        trump_points = self._points_from_trumps()
+        trumps = self._have_trump()
         winning_cards = []
         for card in self._hand:
             if card.suit in trumps:
@@ -252,7 +222,7 @@ class Computer(Player):
             return 0
 
     def make_a_bid(self, opponent_bid):
-        max_points = self.possible_points(opponent_bid)
+        max_points = self._possible_points(opponent_bid)
         if max_points != 0:
             bid = opponent_bid + choice([10, 20])
             points = min(bid, max_points)
@@ -263,20 +233,14 @@ class Computer(Player):
     def choose_musik(self):
         return randint(0, 1)
 
-    def give_card(self, number, opponent):
-        card = self._hand[number]
-        self._hand.remove(card)
-        opponent.add_card(card)
-        return card
-
     def remove_after_musik(self):
-        cards = self.choose_to_remove()
+        cards = self._choose_to_remove()
         for card in cards:
             self._hand.remove(card)
         return cards
 
-    def choose_to_remove(self):
-        trumps = self.have_trump()
+    def _choose_to_remove(self):
+        trumps = self._have_trump()
         points = [0, 2, 3, 4, 10, 11]
         cards_to_remove = []
         names = ["Queen", "King"]
@@ -336,13 +300,7 @@ class Game:
         self._musiki[0]._cards = musik_1
         self._musiki[1]._cards = musik_2
 
-    def player_won_bid(self):
-        if self._player.bid > self._computer.bid:
-            return True
-        else:
-            return False
-
-    def trump_value(self, trump):
+    def _trump_value(self, trump):
         suits = {'Spades': 40, 'Hearts': 100, 'Clubs': 60, 'Diamonds': 80}
         return suits.get(trump)
 
@@ -357,23 +315,23 @@ class Game:
             result = 'p'
         return final_p_points, final_c_points, result
 
-    def set_trump(self, trump):
+    def _set_trump(self, trump):
         self._trump = trump
 
     def set_round(self, new_round):
         self._round = new_round
 
     def set_trumps_for_players(self):
-        self._player.set_trumps()
-        self._computer.set_trumps()
+        self._player._set_trumps()
+        self._computer._set_trumps()
 
     def check_played_card(self, p_card, c_card):
         base_suit = c_card.suit
         if p_card.suit == base_suit or p_card.suit == self.active_trump:
             return False
         else:
-            if (self._player.suit_in_hand(c_card.suit) or
-               self._player.suit_in_hand(self.active_trump)):
+            if (self._player._suit_in_hand(c_card.suit) or
+               self._player._suit_in_hand(self.active_trump)):
                 return True
             else:
                 return False
@@ -381,9 +339,9 @@ class Game:
     def check_declaration(self, p_card, player):
         is_trump_card = (p_card.suit in player._trumps)
         if is_trump_card and p_card.name in ['Queen', 'King']:
-            self.set_trump(p_card.suit)
-            player.trump_played(p_card.suit)
-            player.add_points(self.trump_value(p_card.suit))
+            self._set_trump(p_card.suit)
+            player._trump_played(p_card.suit)
+            player._add_points(self._trump_value(p_card.suit))
             return p_card.suit
         else:
             return None
@@ -415,18 +373,18 @@ class Game:
         if self._round == 'p':
             winning_card = self._cards_battle(player_card, computer_card)
             if winning_card == player_card:
-                self._player.add_points(points_for_win)
+                self._player._add_points(points_for_win)
                 next_round = 'p'
             else:
-                self._computer.add_points(points_for_win)
+                self._computer._add_points(points_for_win)
                 next_round = 'c'
         else:
             winning_card = self._cards_battle(computer_card, player_card)
             if winning_card == computer_card:
-                self._computer.add_points(points_for_win)
+                self._computer._add_points(points_for_win)
                 next_round = 'c'
             else:
-                self._player.add_points(points_for_win)
+                self._player._add_points(points_for_win)
                 next_round = 'p'
         return next_round
 
--- game.py
+++ game.py
@@ -9,14 +9,14 @@ from rich.text import Text
 from rich.table import Table
 
 
-def clear():
+def _clear():
     os.system('clear')
     markdown = Markdown("""# Welcome to the game "1000"!""")
     Console().print(markdown)
 
 
-def clear_in_game(game):
-    clear()
+def _clear_in_game(game):
+    _clear()
     suits = game.suits_dict()
     if game.active_trump:
         trump = game.active_trump
@@ -27,22 +27,22 @@ def clear_in_game(game):
         Console().print("\n")
 
 
-def is_exit(inputted_data):
+def _is_exit(inputted_data):
     if inputted_data.lower() == 'exit':
         os.system('clear')
         exit()
 
 
-def input_cards_to_discard(game):
+def _input_cards_to_discard(game):
     bad_input = True
     while bad_input:
         try:
-            Console().print(cards_with_emoji(game.player))
+            Console().print(_cards_with_emoji(game.player))
             Console().print("")
             info = "Choose two cards to discard (number, number)(1-12):"
             Console().print(info, style="magenta b")
             cards = input(">")
-            is_exit(cards)
+            _is_exit(cards)
             cards = cards.split(", ")
             card_1 = int(cards[0])
             card_2 = int(cards[1])
@@ -56,22 +56,22 @@ def input_cards_to_discard(game):
             Console().print('I do not know what you mean.'
                             'Try again with "number, number".', style="b red")
             sleep(2)
-            clear()
+            _clear()
     return cards_numbers
 
 
-def choose_card_input(game):
+def _choose_card_input(game):
     bad_input = True
     while bad_input:
         try:
             Console().print("")
-            Console().print(cards_with_emoji(game.player))
+            Console().print(_cards_with_emoji(game.player))
             Console().print("")
             info = f'Choose a card to play(1-{game.player.cards_in_hand}): '
             Console().print(info, style="magenta b")
             cards_amount = game._player.cards_in_hand
             chosen_card = input(">")
-            is_exit(chosen_card)
+            _is_exit(chosen_card)
             chosen_card = int(chosen_card)
             if chosen_card in range(1, cards_amount+1):
                 bad_input = False
@@ -81,12 +81,12 @@ def choose_card_input(game):
             Console().print("There is no card with this number. Try again.",
                             style="b red")
             sleep(2)
-            clear_in_game(game)
+            _clear_in_game(game)
     return chosen_card - 1
 
 
-def choose_card_for_card_input(game, played_c_card, cplayed):
-    clear_in_game(game)
+def _choose_card_for_card_input(game, played_c_card, cplayed):
+    _clear_in_game(game)
     invalid_card = True
     bad_input = True
     while bad_input or invalid_card:
@@ -94,12 +94,12 @@ def choose_card_for_card_input(game, played_c_card, cplayed):
             Console().print("")
             Console().print(cplayed)
             Console().print("")
-            Console().print(cards_with_emoji(game.player))
+            Console().print(_cards_with_emoji(game.player))
             info = f'Choose a card to play(1-{game.player.cards_in_hand}): '
             Console().print(info, style="magenta b")
             cards_amount = game._player.cards_in_hand
             card_number = input(">")
-            is_exit(card_number)
+            _is_exit(card_number)
             card_number = int(card_number)
             if card_number in range(1, cards_amount+1):
                 bad_input = False
@@ -113,32 +113,32 @@ def choose_card_for_card_input(game, played_c_card, cplayed):
                 info = "Wrong card. Try card with simmilar suit or with trump"
                 Console().print(info, style="b red")
                 sleep(2)
-                clear_in_game(game)
+                _clear_in_game(game)
         except Exception:
             Console().print("There is no card with this number. Try again.",
                             style="b red")
             sleep(2)
-            clear_in_game(game)
+            _clear_in_game(game)
     return card_number
 
 
-def next_round_text(next_round, game):
+def _next_round_text(next_round, game):
     sleep(1)
     if next_round == 'p':
         Console().print('You won!', style="b bright_green underline")
     else:
         Console().print('Opponent won', style="b red underline")
     sleep(2.5)
-    clear_in_game(game)
+    _clear_in_game(game)
 
 
-def input_musik():
+def _input_musik():
     bad_input = True
     while bad_input:
         try:
             Console().print('Choose a musik to get(1,2): ', style="magenta b")
             chosen_musik = str(input(">").strip())
-            is_exit(chosen_musik)
+            _is_exit(chosen_musik)
             chosen_musik = int(chosen_musik) - 1
             if chosen_musik in [0, 1]:
                 bad_input = False
@@ -148,25 +148,25 @@ def input_musik():
             Console().print("There is no musik with this number. Try again.",
                             style="b red")
             sleep(2)
-            clear()
+            _clear()
     return chosen_musik
 
 
-def card_colored(card):
+def _card_colored(card):
     suits = Game(0, 0, 0, 0).suits_dict()
     card_c = Text(f'{card.name}{suits[card.suit][0]}',
                   style=suits[card.suit][1])
     return card_c
 
 
-def cards_with_emoji(player):
+def _cards_with_emoji(player):
     list_of_cards = player.hand
     number = 1
     joined = Text()
     joined.append("Your cards: ", style="blue bold")
     for card in list_of_cards:
         joined.append(f"{number}.", style="white")
-        joined.append(card_colored(card))
+        joined.append(_card_colored(card))
         if number != len(list_of_cards):
             joined.append(", ", style="white")
         else:
@@ -175,33 +175,33 @@ def cards_with_emoji(player):
     return joined
 
 
-def waiting_for_opponent():
+def _waiting_for_opponent():
     Console().print("Waiting for opponent", end="", style="b white")
     sleep(0.5)
-    for i in range(3):
+    for i in range(2):
         Console().print(".", end="")
         sleep(0.5)
     Console().print(".")
     sleep(0.5)
 
 
-def text_c_played(played_c_card):
+def _text_c_played(played_c_card):
     cplayed = Text()
     cplayed.append('Opponent played: ', style="blue b")
-    cplayed.append(card_colored(played_c_card))
+    cplayed.append(_card_colored(played_c_card))
     cplayed.append('.', style="white")
     return cplayed
 
 
-def text_p_played(played_p_card):
+def _text_p_played(played_p_card):
     played = Text()
     played.append('You played: ', style="blue b")
-    played.append(card_colored(played_p_card))
+    played.append(_card_colored(played_p_card))
     played.append('.', style="white")
     return played
 
 
-def bidding(game):
+def _bidding(game):
     list_of_bids = [str(element) for element in list(range(100, 361))]
     list_of_bids = list_of_bids[::10]
     list_of_bids.append("pass")
@@ -210,8 +210,8 @@ def bidding(game):
     not_passed = True
     computer_bid = 0
     while not_passed:
-        clear()
-        Console().print(cards_with_emoji(game.player))
+        _clear()
+        Console().print(_cards_with_emoji(game.player))
         if computer_bid != 0:
             bid = Text(f"Opponent bidded: {computer_bid}", style="chartreuse3")
             bid.align('right', os.get_terminal_size()[0])
@@ -222,11 +222,11 @@ def bidding(game):
                      style="bold underline wheat1")
         Console().print(quest)
         player_bid = input(">")
-        is_exit(player_bid)
+        _is_exit(player_bid)
         if str(player_bid).lower() == "pass":
             Console().print("You passed", style="b orange_red1")
             sleep(1)
-            clear()
+            _clear()
             player.set_bid(0)
             game.set_round('c')
             not_passed = False
@@ -238,7 +238,7 @@ def bidding(game):
                     " higher than opponent or pass!")
             Console().print(info, style="b red")
             sleep(3)
-            clear()
+            _clear()
             continue
         else:
             player.set_bid(int(player_bid))
@@ -255,87 +255,87 @@ def bidding(game):
             computer.set_bid(0)
             not_passed = False
             sleep(1)
-            clear()
-            chosen_musik = input_musik()
+            _clear()
+            chosen_musik = _input_musik()
             sleep(1)
-            clear()
+            _clear()
     return chosen_musik
 
 
-def play_round(game):
+def _play_round(game):
     if game.round == 'p':
-        card_number = choose_card_input(game)
+        card_number = _choose_card_input(game)
         played_p_card = game.player.play_card(card_number)
         sleep(0.5)
-        clear_in_game(game)
+        _clear_in_game(game)
         Console().print("")
-        played = text_p_played(played_p_card)
+        played = _text_p_played(played_p_card)
         Console().print(played)
         new_trump = game.check_declaration(played_p_card, game.player)
         sleep(0.5)
         if new_trump:
-            clear_in_game(game)
+            _clear_in_game(game)
             Console().print("")
             Console().print(played)
             sleep(0.5)
-        waiting_for_opponent()
-        clear_in_game(game)
+        _waiting_for_opponent()
+        _clear_in_game(game)
         played_c_card = game.computer.make_move(game, played_p_card)
         Console().print("")
         Console().print(played)
-        cplayed = text_c_played(played_c_card)
+        cplayed = _text_c_played(played_c_card)
         Console().print(cplayed)
         next_round = game.battle(played_p_card, played_c_card)
-        next_round_text(next_round, game)
+        _next_round_text(next_round, game)
     else:
-        clear_in_game(game)
+        _clear_in_game(game)
         played_c_card = game.computer.make_move(game)
         Console().print("")
-        cplayed = text_c_played(played_c_card)
+        cplayed = _text_c_played(played_c_card)
         Console().print(cplayed)
         new_trump = game.check_declaration(played_c_card, game.computer)
         sleep(0.5)
         if new_trump:
-            clear_in_game(game)
+            _clear_in_game(game)
             Console().print("")
             Console().print(cplayed)
             sleep(0.5)
         sleep(0.5)
-        card_number = choose_card_for_card_input(game, played_c_card, cplayed)
+        card_number = _choose_card_for_card_input(game, played_c_card, cplayed)
         played_p_card = game.player.play_card(card_number)
-        clear_in_game(game)
+        _clear_in_game(game)
         Console().print("")
         Console().print(cplayed)
-        played = text_p_played(played_p_card)
+        played = _text_p_played(played_p_card)
         Console().print(played)
         next_round = game.battle(played_p_card, played_c_card)
-        next_round_text(next_round, game)
+        _next_round_text(next_round, game)
     game._round = next_round
 
 
-def starting_player_clear_musik(chosen_musik, game):
+def _starting_player_clear_musik(chosen_musik, game):
     if game.round == 'p':
         game.player.add_from_musik(game.musiki[chosen_musik])
-        cards = input_cards_to_discard(game)
+        cards = _input_cards_to_discard(game)
         card_1, card_2 = game.player.remove_after_musik(cards)
         cards_discarded = Text()
         cards_discarded.append("You discarded: ", style="blue b")
-        cards_discarded.append(card_colored(card_1))
+        cards_discarded.append(_card_colored(card_1))
         cards_discarded.append(", ", style="white")
-        cards_discarded.append(card_colored(card_2))
+        cards_discarded.append(_card_colored(card_2))
         cards_discarded.append(".", style="white")
         Console().print("")
         Console().print(cards_discarded)
         sleep(2)
-        clear()
+        _clear()
     else:
         musik = game.musiki[chosen_musik]
         numbers = {
             1: "first",
             2: "second"
         }
-        cards_colored = (card_colored(musik.cards_in_musik()[0]),
-                         card_colored(musik.cards_in_musik()[1]))
+        cards_colored = (_card_colored(musik.cards_in_musik()[0]),
+                         _card_colored(musik.cards_in_musik()[1]))
         chosen = Text()
         chosen.append(f"Opponent chosen {numbers[chosen_musik+1]} Musik: ",
                       style="b blue")
@@ -347,12 +347,26 @@ def starting_player_clear_musik(chosen_musik, game):
         game.computer.add_from_musik(musik)
         card_1, card_2 = game.computer.remove_after_musik()
         sleep(2)
-        clear()
+        _clear()
     game.set_trumps_for_players()
 
 
+def _intro():
+    _clear()
+    sleep(1)
+    info = Text('At any moment you can type exit to quit the game!',
+                style="bold red frame")
+    info.align('center', os.get_terminal_size()[0])
+    Console().print(info)
+    sleep(2)
+    _clear()
+    for i in track(range(25), description="Dealing cards..."):
+        sleep(0.1)
+    _clear()
+
+
 def summary(game):
-    clear()
+    _clear()
     p_points = game.player.points
     c_points = game.computer.points
     p_bid = game.player.bid if game.player.bid > 0 else "Passed"
@@ -361,14 +375,14 @@ def summary(game):
     table.add_column("", style="cyan b")
     table.add_column("Player", style="magenta")
     table.add_column("Opponent", style="green")
-    table.add_row(f"Bare points{''*10}", str(p_points), str(c_points))
+    table.add_row("Bare points", str(p_points), str(c_points))
     Console().print(table)
     sleep(1)
-    clear()
+    _clear()
     table.add_row("Bids", str(p_bid), str(c_bid))
     Console().print(table)
     sleep(1)
-    clear()
+    _clear()
     final_p_points, final_c_points, result = game.count_final_points()
     table.add_row("Final points(rounded)",
                   str(final_p_points), str(final_c_points))
@@ -382,23 +396,9 @@ def summary(game):
         markdown = Markdown("""# You won!""", style="green")
     Console().print(markdown)
     sleep(1)
-    Console().print("Type enything to exit:", style="magenta")
-    input(">")
-    is_exit("exit")
-
-
-def intro(game):
-    clear()
-    sleep(1)
-    info = Text('At any moment you can type exit to quit the game!',
-                style="bold red frame")
-    info.align('center', os.get_terminal_size()[0])
-    Console().print(info)
-    sleep(2)
-    clear()
-    for i in track(range(25), description="Dealing cards..."):
-        sleep(0.1)
-    clear()
+    Console().print("Click enter to quit:", style="magenta")
+    input()
+    _is_exit("exit")
 
 
 def initialize_game():
@@ -414,8 +414,8 @@ def initialize_game():
 
 def play_game(game):
     game.deal_the_cards()
-    intro(game)
-    chosen_musik = bidding(game)
-    starting_player_clear_musik(chosen_musik, game)
+    _intro()
+    chosen_musik = _bidding(game)
+    _starting_player_clear_musik(chosen_musik, game)
     for i in range(10):
-        play_round(game)
+        _play_round(game)
